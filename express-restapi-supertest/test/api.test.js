b717be6d4f48b36787537e07a69bad6c63fd017c
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2021-01-11 17:25:43 -0500

    create user

--- express-restapi-supertest/test/api.test.js
+++ express-restapi-supertest/test/api.test.js
@@ -32,3 +32,38 @@ describe("GET /users/:id", () => {
       });
   });
 });
+
+// Testing POST users endpoint
+describe("POST /users", () => {
+  it("respond with 201 created", (done) => {
+    const data = {
+      username: "juan",
+      password: "12345",
+    };
+    request(app).post("/users").send(data);
+    set("Accept", "application/json");
+    expect("Content-Type", /json/)
+      .expect(201)
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it("respond with 400 on bad request", (done) => {
+    const data = {
+      // no username and password
+    };
+    request(app)
+      .post("/users")
+      .send(data)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(400)
+      .expect("user not created")
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
